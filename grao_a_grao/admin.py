36eb1701efe0a97a46dd622149e31667fdb6fb6f
beatrizpalma00 <beatrizpalma00@gmail.com>
2026-04-29 11:16:56 +0100

    Refinamento das categorias e tipos de prato
    Adicionada a distinção entre dieta e momento da refeição com menus de seleção.

--- grao_a_grao/admin.py
+++ grao_a_grao/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Receita, Ingrediente
 
-# Register your models here.
+# Os ingredientes aparecem na página da receita
+class IngredienteInline(admin.TabularInline):
+    model = Ingrediente
+    extra = 3  # Linhas vazias para preencher
+
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    inlines = [IngredienteInline]
+    list_display = ('titulo', 'autor', 'data_criacao')
--- grao_a_grao/models.py
+++ grao_a_grao/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Receita(models.Model):
+    # Lista de opções para a Dieta/Base alimentar
+    OPCOES_CATEGORIA = [
+        ('carne', 'Carne'),
+        ('peixe', 'Peixe'),
+        ('vegetariano', 'Vegetariano'),
+        ('vegan', 'Vegan'),
+    ]
+
+    # Lista de opções para o Momento da Refeição
+    OPCOES_TIPO = [
+        ('entrada', 'Entrada'),
+        ('prato_principal', 'Prato Principal'),
+        ('sobremesa', 'Sobremesa'),
+        ('lanche', 'Lanche/Pequeno-almoço'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    # Implementação dos novos campos com escolhas pré-definidas
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default='carne')
+    tipo_prato = models.CharField(max_length=50, choices=OPCOES_TIPO, default='prato_principal')
+    
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    preparacao = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Ingrediente(models.Model):
+    # uma receita, vários ingredientes.
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE, related_name='ingredientes')
+    nome = models.CharField(max_length=100)
+    quantidade = models.DecimalField(max_digits=7, decimal_places=2) # Para dar pra somar números com vírgula
+    unidade = models.CharField(max_length=50) # Ex: gramas, unidades, kg
+
+    def __str__(self):
+        return f"{self.nome} ({self.quantidade} {self.unidade})"
